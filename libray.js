30d66adee6e25e4f5bd8b3cbda043a1af14d0e61
Jovanny <jovannyrebollar1@gmail.com>
2022-01-02 01:04:16 -0500

    Generating the table and table headings

--- libray.js
+++ libray.js
@@ -1,23 +1,47 @@
 let myLibray = [];
 
+let headers = ['title', 'author', 'pages']
+
 const form = document.querySelector('#form');
+const mainTable = document.querySelector('#table')
 
 function getInfo(objEvent) {
     objEvent.preventDefault()
 
+    // creating table
+    let table = document.createElement('table');
+    let headerRow = document.createElement('tr');
+
+    headers.forEach((headerText) => {
+        let header = document.createElement('th');
+        let textNode = document.createTextNode(headerText);
+
+        header.appendChild(textNode);
+        // <tr> <th> text node </th> <tr>
+        headerRow.appendChild(header);
+    });
+
+    // <table> <tr> <th> textNode </th> </tr> </table>
+    table.appendChild(headerRow);
+
+    // <div> <table>
+    mainTable.appendChild(table)
+
+
     // Getting the values
-    let title = document.querySelector('#title').value
-    console.log(title)
-    let author = document.querySelector('#author').value
-    console.log(author)
-    let pages = document.querySelector('#pages').value
-    console.log(pages)
+    let title = document.querySelector('#title').value;
+    console.log(title);
+    let author = document.querySelector('#author').value;
+    console.log(author);
+    let pages = document.querySelector('#pages').value;
+    console.log(pages);
 
 
-    addBookToLibrary(newBook(title,author,pages))
+    addBookToLibrary(newBook(title,author,pages));
 
-    console.table(myLibray)
+    console.table(myLibray);
 
+    clearValues();
 
 }
 
@@ -27,6 +51,12 @@ function Book(title,author,pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+}
+
+function clearValues() {
+    title.value = ''
+    author.value = ''
+    pages.value = ''
 }
 
 function addBookToLibrary(obj) {
@@ -45,7 +75,6 @@ function librayInventory(libray) {
     })
 }
 
-librayInventory(myLibray)
 
 
 
